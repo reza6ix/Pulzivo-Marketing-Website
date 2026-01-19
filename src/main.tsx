76f78901a979216d892011faf9837174404df60d
mo <mo@mos-MacBook-Pro.local>
2026-01-18 19:45:57 -0500

    Fix deployment: Copy CNAME to build artifact

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-console.log('Deployment workflow test');
+console.log('Deployment workflow CNAME fix');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
