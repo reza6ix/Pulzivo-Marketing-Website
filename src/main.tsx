8764358cee06db430b403e7e25dc1e711c342ef2
mo <mo@mos-MacBook-Pro.local>
2026-01-18 19:42:19 -0500

    Force build update

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+console.log('Deployment trigger check');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
