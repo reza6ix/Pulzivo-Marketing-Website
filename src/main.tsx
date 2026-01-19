722651258dccd2ef92f2c6a69bc2f34b3eeb11c0
mo <mo@mos-MacBook-Pro.local>
2026-01-18 19:53:35 -0500

    Revert to gh-pages branch deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-console.log('Deployment workflow CNAME fix');
+console.log('Deployment via gh-pages branch');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
